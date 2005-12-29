743bd76e966b4b93cb32f0d89779f719f7f463a3
rskull <rskull>
2005-12-29 19:12:50 +0000

    *** empty log message ***

--- pruGUI/src/prueba/PanelGrafico.java
+++ pruGUI/src/prueba/PanelGrafico.java
@@ -2,11 +2,40 @@ package prueba;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+
+import org.jfree.chart.*;
+import org.jfree.data.*;
+import org.jfree.data.xy.*;
+import org.jfree.data.category.*;
+import org.jfree.chart.plot.*;
+
 public class PanelGrafico extends JPanel{
 	public void paint(Graphics g){
 		super.paint(g);
-		g.setColor(Color.YELLOW);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.drawImage(crearImagen(), 0, 0, null);
+	}
+	
+	public BufferedImage crearImagen(){
+		XYSeries  serie1 = new XYSeries("Prueba 1");
+		for (int i=1; i< 40; i+=1) serie1.add(i, i);
+
+		XYSeries serie2 = new XYSeries("Prueba 2");
+		for (int i=1; i< 40; i+=1) serie2.add(i, i+3);
 		
+		XYSeriesCollection datos = new XYSeriesCollection();
+		datos.addSeries(serie1);
+		datos.addSeries(serie2);
+		
+		JFreeChart chart = 
+			ChartFactory.createXYLineChart(
+					"Prueba", 
+					"Meses", 
+					"Sesiones", 
+					datos, 
+					PlotOrientation.VERTICAL,
+					false, false, true);
+		BufferedImage bi = chart.createBufferedImage(300,300);
+		return bi;
 	}
 }
--- pruGUI/src/prueba/VentanaPrueba2.java
+++ pruGUI/src/prueba/VentanaPrueba2.java
@@ -1,18 +1,46 @@
 package prueba;
 
 import javax.swing.*;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.awt.*;
+
+import org.jfree.chart.*;
+import org.jfree.data.*;
+import org.jfree.data.xy.*;
+import org.jfree.data.category.*;
+import org.jfree.chart.plot.*;
 
 public class VentanaPrueba2 extends JInternalFrame{
 	public void jbInit(){
-		setSize(300, 300);
+		setSize(400, 400);
 		setVisible(true);
 		setResizable(true);
 		setTitle("Ventana Prueba 2");
 		setClosable(true);
 		setMaximizable(true);
+
+		XYSeries  serie1 = new XYSeries("Prueba 1");
+		for (int i=1; i< 40; i+=1) serie1.add(i, i);
+
+		XYSeries serie2 = new XYSeries("Prueba 2");
+		for (int i=1; i< 40; i+=1) serie2.add(i, i+3);
 		
-		getContentPane().add(new PanelGrafico());
+		XYSeriesCollection datos = new XYSeriesCollection();
+		datos.addSeries(serie1);
+		datos.addSeries(serie2);
+		
+		JFreeChart chart = 
+			ChartFactory.createXYLineChart("","Meses","Sesiones", datos,	PlotOrientation.VERTICAL,false, false, true);
+
+		ChartPanel panel = new ChartPanel(chart);
+		getContentPane().add(panel);
 	}
 }
