b90cdaaa4ac799fd226272a07f7e26a074d25720
itzipg <itzipg>
2006-01-31 18:25:39 +0000

    *** empty log message ***

--- StoXtreme/src/stoxtreme/servidor/objeto_bolsa/informacion/Informacion.java
+++ StoXtreme/src/stoxtreme/servidor/objeto_bolsa/informacion/Informacion.java
@@ -1,5 +1,26 @@
 package stoxtreme.servidor.objeto_bolsa.informacion;
 
-public class Informacion {
+import stoxtreme.interfaz_remota.IInformacion;
 
+public class Informacion {
+	private Balance balance;
+	private InfoBursatil iBursatil;
+	private Cuenta cuenta;
+	
+	public Informacion(Balance b, InfoBursatil ib, Cuenta c){
+		balance=b;
+		iBursatil=ib;
+		cuenta=c;
+	}
+	
+	public Balance getBalance(){
+		return balance;
+	}
+	
+	public InfoBursatil getIBursatil(){
+		return iBursatil;
+	}
+	public Cuenta getCuenta(){
+		return cuenta;
+	}
 }
