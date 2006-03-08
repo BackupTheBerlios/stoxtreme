a59a6417acb548695661423a4f6414e03197f3d3
rskull <rskull>
2006-03-08 10:50:33 +0000

    *** empty log message ***

--- StoXtreme/src/stoxtreme/herramienta_agentes/HerramientaAgentes.java
+++ StoXtreme/src/stoxtreme/herramienta_agentes/HerramientaAgentes.java
@@ -4,28 +4,15 @@ import java.util.*;
 
 import stoxtreme.herramienta_agentes.agentes.AgentePrueba;
 
-public class HerramientaAgentes extends Thread{
-	ArrayList listaAgentes;
+public class HerramientaAgentes{
+	ArrayList<Agente> listaAgentes;
 	MonitorAgentes monitor;
 	
 	public HerramientaAgentes(ParametrosAgentes parametros){
-		
+		listaAgentes = new Array
 	}
 	
 	public void cambiaParametros(ParametrosAgentes nParam){
 		
-	}
-	
-	public void run(){
-		Iterator it = listaAgentes.iterator();
-		
-		while(it.hasNext()){
-			((AgentePrueba)it.next()).start();
-		}
-		
-		monitor.start();
-		while(true){
-			
-		}
 	}
 }
