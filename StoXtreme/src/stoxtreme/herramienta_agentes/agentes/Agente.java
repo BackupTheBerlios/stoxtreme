1b386f610aabc9bc8c000adc2d443d9d55416da9
rskull <rskull>
2006-05-15 14:37:51 +0000

    *** empty log message ***

--- StoXtreme/src/stoxtreme/herramienta_agentes/agentes/Agente.java
+++ StoXtreme/src/stoxtreme/herramienta_agentes/agentes/Agente.java
@@ -14,14 +14,6 @@ import stoxtreme.herramienta_agentes.agentes.interaccion_agentes.MensajeACL;
 import stoxtreme.interfaz_remota.Operacion;
 import stoxtreme.interfaz_remota.Stoxtreme;
 import stoxtreme.servidor.Servidor;
-// Primer prototipo de agente broker.
-// Inicialmente se le introduce el parametro correspondiente al tiempo de espera
-// El broker, esperará a que transcurran tiempoEspera ciclos de ejecución y cuando
-// supere el valor efectuará una acción aleatoria, dependiendo de la acción que hubiera
-// realizado antes y siempre con el precio actual de mercado.
-// PreStep - Analiza el entorno y guarda los datos necesrios
-// Step - Con los datos anteriores modifica sus intenciones
-// PostStep - Introduce la operación en el sistema correspondiendo con sus intenciones
 
 public class Agente extends Thread{
 	private IDAgente ID;
@@ -112,33 +104,41 @@ public class Agente extends Thread{
 		alive = false;
 	}
 	public void insertarOperacion(Operacion o){
-		String empresa = o.getEmpresa();
-		int tipo = o.getTipoOp();
-		int numAcciones = o.getCantidad();
-		double precio = o.getPrecio();
-		
-		
-		try {
-			int i = conexionBolsa.insertarOperacion(this.ID.toString(), o);
-			
-			if(i!= -1){
-				opPendientes.insertaOperacionPendiente(i, empresa, tipo, numAcciones, precio);
+		if(alive){
+			String empresa = o.getEmpresa();
+			int tipo = o.getTipoOp();
+			int numAcciones = o.getCantidad();
+			double precio = o.getPrecio();
+
+			try {
+				int i = conexionBolsa.insertarOperacion(this.ID.toString(), o);
+				if(i!= -1){
+					opPendientes.insertaOperacionPendiente(i, empresa, tipo, numAcciones, precio);
+				}
+			} catch (RemoteException e) {
+				e.printStackTrace();
 			}
-		} catch (RemoteException e) {
-			e.printStackTrace();
 		}
 	}
 	
 	public void cancelarOperacion(int operacion){
-		try {
-			conexionBolsa.cancelarOperacion(this.ID.toString(),operacion);
-		} catch (RemoteException e) {
-			e.printStackTrace();
+		if(alive){
+			try {
+				conexionBolsa.cancelarOperacion(this.ID.toString(),operacion);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public String getIDString(){
 		return ID.toString();
+	}
+	
+	public void abandonarModelo(){
+		if(alive){
+			
+		}
 	}
 	
 	public void enviarMensaje(MensajeACL m){
